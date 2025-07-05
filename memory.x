5d9de4fb5d948bcaa0241b3af9ed5113752fd379
Matthew Cantrill <cantrillmatt@gmail.com>
2025-07-05 10:48:02 +1000

    created main program setup data, documented all current code

--- memory.x
+++ memory.x
@@ -1,23 +1,70 @@
+/*
+This first section simply tells the compiler where each section of memory is 
+addressed, as well as it's size. for our initial layout, we allowing access
+to the entire flash memory and ram
+*/
 MEMORY {
     FLASH (rx) : ORIGIN = 0x00000000, Length = 1M
     RAM (rwx) : ORIGIN = 0x1FFF0000, LENGTH = 256K
 }
 
+/*
+This section is how we describe the location of our program and it's data. 
+*/
 SECTIONS {
+    /*
+    The .text section is where all the program code should be placed. for this 
+    embedded implementation, code should be stored in flash, as well as some other
+    data that is required for the function of the processor
+    */
     .text : {
+        /*
+        Cortex-M processors have a vector table listed at the start of flash. The
+        first address, 0x0000_0000, is the stack pointer and the second address,
+        0x0000_0004 is the program pointer. the stack pointer is defined as the address
+        at the end of ur ram, as it is a descending stack. the reset vector is defined
+        in main.rs
+        */
         . = 0;
-        KEEP(*(.vectors))
+        LONG(ORIGIN(RAM) + LENGTH(RAM))
+        KEEP(*(.reset_vector))
+        /*
+        The next section we need to describe is the flash config. The cortex processor
+        has a 16 byte flash configuration stored at 0x0000_0400. This describes any 
+        restrictions to the flash module
+        */
         . = 0x400;
         KEEP(*(.flashconfig*))
+        /*
+        Once the vector table and flash config are defined, we can write our program
+        code. This needs to start at a x4 alilgned address, as the processor accesses
+        data in 4 byte sections.
+        */
         . = ALIGN(4);
         *(.text*)
+    /*
+    The > FLASH = 0xFF descriptors tell the compiler to place all the above data into 
+    the flash section of memory, and to fill any blank space with the value 0xFF. this 
+    value is chosen as Flash is erased to 0xFF and then writing will flip the required 
+    bits as needed to reduce the number of writes to the flash.
+    */
     } > FLASH = 0xFF
 
+    /*
+    The rodata section describes all the data that is read only and will not be changed
+    */
     .rodata : {
         *(.rodata*)
+    /*
+    all rodata should be stored in flash for permanence. this can be read to ram as needed
+    */
     } > FLASH
 
-    /DISCAD/ : {
+    /*
+    The /DISCARD/ section simply tells the compiler not to include this data in the final 
+    binary
+    */ 
+    /DISCARD/ : {
         *(.arm*)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,63 @@
-fn main() {
-    println!("Hello, world!");
+// these directives tell the rust compiler not to use the standard library and not to 
+// expect a main function entry point. this allows us to remove overhead we can't use
+// from the standard library, and to define our own program entry.
+#![no_main]
+#![no_std]
+
+// the core panicinfo datatype needs to be included for panic handling
+use core::panic::PanicInfo;
+
+// we also need to include all of our chip access files
+mod port;
+
+// we define our reset function that will run whenever the chip is reset. the no_mangle 
+// directive tells the compiler to ensure the symbol defined for the function does not change.
+// In this case, our symbol will be called reset, and we can then use this to point our
+// reset vector to this function
+#[unsafe(no_mangle)]
+pub unsafe extern "C" fn Reset() -> ! {
+    // in our reset function, we will simply define and then run our main function. this
+    // must be declared unsafe, as the compiler cannot complete borrow checking on external
+    // functions
+    unsafe extern "Rust" {
+        unsafe fn main() -> !;
+    }
+
+    // this is where we call our main function
+    unsafe {
+        main();
+    }
+
+    // if main exits, we want to loop to prevent further execution
+    loop {}
 }
+
+// wwe must also define the reset vector in order to tell the compiler where our entry point will
+// be
+#[unsafe(link_section = ".reset_vector")]
+#[unsafe(no_mangle)]
+pub static RESET_VECTOR: unsafe extern "C" fn() -> ! = Reset;
+
+// we also need to define our flash config. This is done by referencing the 16 byte values in the
+// cortex datasheet. The layout is as follows
+// First 8 bytes is backdoor access Key. this is not used so should have all bytes defaulted 
+// to 0xFF. The next 4 bytes handle flash protection. for our purposes we do not want to allow
+// any flash protection so these should be filled as 0xFF. The final 4 bytes, refer to FlexNVM
+// flash data protection, FlexNVM EEPROM data protection, flash options, and flash security
+// respectively. We do not need any data protection so the FlexNVM registers can be set to 0xFF,
+// we want all the flash options enabled, such as nmi interrupts and the boot mode. finally, we
+// want the MCU to be secure and the ability to mass erase the flash, so our final byte should
+// also be set to 0xFF
+#[unsafe(link_section = ".flashconfig")]
+#[unsafe(no_mangle)]
+pub static FLASH_CONFIG: [u8; 16] = [0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF];
+
+// The panic_handler directive tells the compiler the function to run when a panic occurs.
+// In this case, as we have no way to alert the user currently, we can simple make the program
+// enter an endless loop to prevent further execution of the program
+#[panic_handler]
+fn panic(_panic: &PanicInfo<'_>) -> ! {
+    loop{}
+}
+
+// Now all the setup is complete, we can write our main function for execution
